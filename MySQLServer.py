8aec3c8017565173c8eeae1da8d9bdf399a9697f
Hillary Robert <roberthillary30@gmail.com>
2025-10-19 11:23:01 +0300

    Lets Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,26 +1,23 @@
 import mysql.connector
 
-from mysql.connector import Error
-
-
-try: 
-  mydb = mysql.connector.connect (
-    host = "localhost",
-    user = "root",
-    password = "Rahbode25"
+try:
+    # Connect to MySQL server
+  mydb = mysql.connector.connect(
+      host="localhost",
+      user="root",
+      password="Rahbode25"
   )
 
   if mydb.is_connected():
     cursor = mydb.cursor()
     cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
 
-    print("DATABASE 'alx_book_store' created successfully")
-
-except Error as e:
-  print(f"Error while connecting to mySQL: {e}")
+except mysql.connector.Error as e:
+  print(f"Error while connecting to MySQL: {e}")
 
 finally:
-  if "cursor" in locals():
-    cursor.close()
-  if "mydb" in locals() and mydb.is_connected():
-    mydb.close()
+  if 'cursor' in locals():
+      cursor.close()
+  if 'mydb' in locals() and mydb.is_connected():
+      mydb.close()
